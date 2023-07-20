5c12246479ab4ee2624d81cf0bb6f05ba61d9006
Christopher Mayes <31023527+ChristopherMayes@users.noreply.github.com>
2023-07-20 10:06:47 -0700

    proper conda-forge style patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -44,10 +44,6 @@ EOF
 fi
 echo "***DEBUG CMAKE_ARGS: ${CMAKE_ARGS}"
 
-echo "***DEBUG Patching CMAKE_ARGS into util/mk-mkd"
-#sed -i "s/cmake -DCMAKE_BUILD_TYPE/cmake \${CMAKE_ARGS} -DCMAKE_BUILD_TYPE/" util/mk-mkd
-patch util/mk-mkd ${RECIPE_DIR}/mk-mkd.patch
-
 echo "**** Invoking dist_source_me"
 source util/dist_source_me
 
