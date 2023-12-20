c7544a8f823995d7de97db7b2b3a19d7d1125ec6
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2023-12-20 11:58:39 -0800

    FIX: add plplot to include list

--- recipe/build.sh
+++ recipe/build.sh
@@ -72,6 +72,7 @@ fi
 
 # Do not build plplot on any platform; rely on the conda-forge build.
 patch -p1 < "${RECIPE_DIR}/skip-plot-build.patch"
+patch -p1 < "${RECIPE_DIR}/add-plplot-to-include.patch"
 
 # build production if BUILD_PRODUCTION is set to Y
 if [[ "$BUILD_PRODUCTION" == "Y" ]]; then
